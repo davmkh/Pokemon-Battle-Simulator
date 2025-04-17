f7ba6ff766cb258066e80827aae3bd301988d532
Davit Mkhoyan <dddavitdd@gmail.com>
2025-04-17 12:53:57 -0700

    Pokemon base class (worked on) Created Stats class Created Characters class

---
+++ Pokemon/Stats.cpp
@@ -0,0 +1,43 @@
+#include "Stats.h"
+
+Stats::Stats(int Wins, int Losses, int RoundsPlayed) {
+	wins = Wins;
+	losses = Losses;
+	roundsPlayed = RoundsPlayed;
+}
+
+// increment functions
+void Stats::incrementWins() {
+	wins++;
+}
+
+void Stats::incrementLosses() {
+	losses++;
+}
+
+void Stats::incrementRoundsPlayed() {
+	roundsPlayed++;
+}
+
+// getters
+int Stats::getWins() {
+	return wins;
+}
+
+int Stats::getLosses() {
+	return losses;
+}
+
+int Stats::getRoundsPlayed() {
+	return roundsPlayed;
+}
+
+// file handling functions
+void Stats::loadStats() {
+
+}
+
+void Stats::saveStats() {
+
+}
+
---
+++ Pokemon/Stats.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "header.h"
+
+class Stats {
+private:
+	// tracks user data
+	int wins;
+	int losses;
+	int roundsPlayed;
+
+public:
+	// constructor
+	Stats(int Wins, int Losses, int RoundsPlayed);
+	
+	// triggers at the end of a match
+	void incrementWins();
+	void incrementLosses();
+	void incrementRoundsPlayed();
+
+	// getters
+	int getWins();
+	int getLosses();
+	int getRoundsPlayed();
+
+	// file handling functions
+	void loadStats();
+	void saveStats();
+
+
+
+
+};
+
