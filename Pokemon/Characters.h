f911becfe77cfc5aef0ba245d6b8f48380031f3f
Davit Mkhoyan <dddavitdd@gmail.com>
2025-04-17 13:09:04 -0700

    Characters class updated

--- Pokemon/Characters.h
+++ Pokemon/Characters.h
@@ -2,15 +2,57 @@
 #include "header.h"
 #include "Pokemon.h"
 
-// Pikachu
+// Pikachu - Electric
 class Pikachu : public Pokemon {
 
 };
 
-// Charizard
+// Charizard - Fire
 class Charizard : public Pokemon {
 
 };
 
+// Venasaur - Grass
+class Venasaur : public Pokemon {
 
+};
 
+// Blastoise - Water
+class Blastoise : public Pokemon {
+
+};
+
+// Abomasnow - Ice
+class Abomasnow : public Pokemon {
+
+};
+
+// Gengar - Ghost
+class Gengar : public Pokemon {
+
+};
+
+// Darkrai - Dark
+class Darkrai : public Pokemon {
+
+};
+
+// Lucario - Fighting
+class Lucario : public Pokemon {
+
+};
+
+// Metagross - Steel
+class Metagross : public Pokemon {
+
+};
+
+// Mewtwo - Pyschic
+class Mewtwo : public Pokemon {
+
+};
+
+// Abomasnow - Ice
+class Abomasnow : public Pokemon {
+
+};
